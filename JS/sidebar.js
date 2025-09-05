e63d55af245dc98b5e598ac91bebe009f4448091
chris tutje <chris.tutje@gmail.com>
2025-09-05 15:34:15 -0500

    gddfg
    dfggfdgdf

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -1,14 +1,3 @@
-// function toggleSidebar() {
-//     const sidebar = document.getElementById("sidebar");
-//     const currentWidth = sidebar.style.width;
-
-//     if (currentWidth === "250px") {
-//         sidebar.style.width = "0";
-//     } else {
-//         sidebar.style.width = "250px";
-//     }
-// }
-
 const sidebar = document.getElementById("sidebar");
 const toggleButton = document.querySelector(".toggle-sidebar-button");
 
